d3dee84b0a50f60ac6efefeadccf476559d273ec
sakamoto5 <192305395+sakamoto5@users.noreply.github.com>
2025-01-07 18:26:20 +0900

    addを３回繰り返した

--- git-practice.cpp
+++ git-practice.cpp
@@ -7,5 +7,9 @@ using namespace std;
 int main(){
     cout<< "hello world\n";
     cout<< "2回目のpush\n";
+    //=====================
+    cout<< "addの１回目\n";
+    cout<< "addの２回目\n";
+    cout<< "addの３回目\n";
     return 0;
 }
